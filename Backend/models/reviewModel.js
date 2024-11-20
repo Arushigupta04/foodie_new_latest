f0a93b3b76e2b6a25a706ef71ecc4ecc09ec0e3d
Arushigupta04 <122527185+Arushigupta04@users.noreply.github.com>
2024-11-20 10:57:21 +0530

    Create reviewModel.js

---
+++ Backend/models/reviewModel.js
@@ -0,0 +1,25 @@
+const mongoose=require("mongoose")
+const reviewSchema = new mongoose.Schema({
+    email: {
+      type: String,
+      required: true,
+      match: /^[^\s@]+@[^\s@]+\.[^\s@]+$/, // Email validation regex
+    },
+    rating: {
+      type: Number,
+      required: true,
+      min: 1,
+      max: 5,
+    },
+    reviewText: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+  });
+  
+  module.exports = mongoose.model("Review", reviewSchema);
+  
